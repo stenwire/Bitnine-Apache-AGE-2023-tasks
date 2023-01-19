5402f3695ba1615d3da77f9e2dc1b05e9518b523
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-19 19:29:28 +0100

    fix/fix syntax

--- task-3-Driver(Inteface)/setup.sql
+++ task-3-Driver(Inteface)/setup.sql
@@ -7,5 +7,5 @@ phone character varying(20) COLLATE pg_catalog."default",
 CONSTRAINT user_table_pkey PRIMARY KEY (user_id)
 );
 INSERT INTO public.user_table (user_id, name, age, phone) VALUES (3, 'Jenny', 34, NULL);
-INSERT INTO public.user_table (user_id, name, age, phone) VALUES (2, 'Tom', 29, '1-800-123-
-1234'); INSERT INTO public.user_table (user_id, name, age, phone) VALUES (1, 'John', 28, NULL);
+INSERT INTO public.user_table (user_id, name, age, phone) VALUES (2, 'Tom', 29, '1-800-123-1234');
+INSERT INTO public.user_table (user_id, name, age, phone) VALUES (1, 'John', 28, NULL);
