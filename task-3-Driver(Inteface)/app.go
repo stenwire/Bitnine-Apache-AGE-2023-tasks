452011c3c8673fc6e0206ecc1f58ed800cf42c1c
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-29 11:40:04 +0100

    chore: fix-env-varibles-and-connection-to-db

--- task-3-Driver(Inteface)/app.go
+++ task-3-Driver(Inteface)/app.go
@@ -1,31 +1,32 @@
 package main
-// querying all rows from the database with Go lang sql package
+
 import (
-    "database/sql"
-    "fmt"
-	"os"
+	"database/sql"
+	"fmt"
 	"log"
-	"strconv"
+	"os"
 	"github.com/joho/godotenv"
-    _ "github.com/lib/pq"
-)
-const (
-    host = "localhost"
-    port = 5432
-    user = "postgres"
-    password = "despicable01"
-    dbname = "bitnine"
-)
-var (
-    ename string
-    sal int
+	_ "github.com/lib/pq"
 )
 
-// use godot package to load/read the .env file and
-// return the value of the key
+const (
+	host = ""
+	port = 5432
+	user = ""
+	password = ""
+	dbname = ""
+)
+
+type sandbox struct {
+	user_id int
+	name string
+	age int
+	phone string
+}
+
+// load .env file
 func goDotEnvVariable(key string) string {
 
-	// load .env file
 	err := godotenv.Load(".env")
 
 	if err != nil {
@@ -35,50 +36,22 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func goDotEnvVariableInt(key string) int {
-
-	// load .env file
-	// err := godotenv.Load(".env")
-
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// }
-
-	// return os.Getenv(key)
-
-	val := goDotEnvVariableInt(key)
-	ret, err := strconv.Atoi(val)
-    if err != nil {
-        panic(fmt.Sprintf("some error"))
-    }
-    return ret
-
-}
-
+// main function
 func main() {
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-        "password=%s dbname=%s sslmode=disable",
-        goDotEnvVariable(host), goDotEnvVariableInt(port_go), goDotEnvVariable(user), goDotEnvVariable(password), goDotEnvVariable(dbname))
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	goDotEnvVariable("host"), port, goDotEnvVariable("user"), goDotEnvVariable("password"), goDotEnvVariable("dbname"))
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-    rows, err := db.Query("SELECT user_id, name, age, phone FROM user_table ORDER BY user_id")
-    if err != nil {
-        panic(err)
-    }
-    defer rows.Close()
-    for rows.Next() {
-        err := rows.Scan(&ename, &sal)
-        if err != nil {
-            panic(err)
-        }
-        fmt.Println("\n", ename, sal)
-    }
-    err = rows.Err()
-    if err != nil {
-        panic(err)
-    }
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
+
